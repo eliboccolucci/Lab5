cd82453a4a9be629d806eb79bb81d34e98f7959a
Devin Evangelista <120211919+Devinevangelista@users.noreply.github.com>
2024-03-06 14:43:28 -0500

    Update IFish.java
    added Fish Interface

--- IFish.java
+++ IFish.java
@@ -1,16 +1,23 @@
 /**
+ * Interface that extends IFish and adds methods for Fish
  *
+ * @defines numFins, the number of fins
  *
- * @defines
+ * @constraints numFins >= 0
  *
- * @constraints
- *
- * @initialization_ensures
+ * @initialization_ensures a fish is created with fins, genus, species, and commonName
  */
 public interface IFish extends IAnimal
 {
     /**
+     * getter function that returns fin count
      *
+     * @return The fin count as an int
+     *
+     * @pre none
+     *
+     * @post getFinCount = finCount AND commonName = #commonName AND genus = #genus AND species = #species
+     *       AND finCount = #finCount
      */
     public int getFinCount();
 }
