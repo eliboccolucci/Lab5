122fdc0683cf85815955d80a90e02ebc55a02966
Eli Boccolucci <148370535+eliboccolucci@users.noreply.github.com>
2024-03-05 20:38:20 -0500

    Update IMammal.java

--- IMammal.java
+++ IMammal.java
@@ -1,16 +1,19 @@
 /**
- *
+ * Interface that extends the IAnimal inteface and adds methods specific to mammals
  *
  * @defines
+ *      #LegCount       // Counts the amount of legs the mammal has
  *
  * @constraints
+ *      legCount > 0k
  *
  * @initialization_ensures
+ *
  */
 public interface IMammal extends IAnimal
 {
     /**
-     *
+     * @return The number of legs the mammal has
      */
     public int getLegCount();
 }
